b8f078f204eb956aa4a72eee4afda40ed12228e9
Prince <princeoguignatius@gmail.com>
2024-10-30 14:58:29 +0000

    add to cart

--- main.js
+++ main.js
@@ -62,10 +62,58 @@ const initApp = () => {
            <img src="images/${value.images}">
            <div class="title">${value.name}</div>
            <div class="price">${value.price.toLocaleString()}</div>
-           <button onclick="addToCard(${key})">Add To Card</button>
+           <button onclick="addToCard(${key})">Add To Cart</button>
         `
         list.appendChild(newDiv)
 })
 }
 
 initApp() 
+
+const addToCard = (key) => {
+    if(listCards[key] == null){
+        listCards[key] = products[key];
+        listCards[key].quantity = 1
+    }
+
+    reloadCard();
+}
+const reloadCard = () => {
+    listCard.innerHTML = "";
+    let count = 0;
+    let totalPrice = 0;
+
+    listCards.forEach((value, key) => {
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+
+        if(value != null){
+            let newDiv = document.createElement("li");
+            newDiv.innerHTML = `
+                <div><img src ="images/${value.images}"></div>
+                <div class="cardTitle">${value.name}</div>
+                <div class="cardPrice">${value.price.toLocaleString()}</div>
+
+                <div>
+                <button style="background-color: #560bad" class="cardButton" onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                <div class="count">${count}</div>
+                <button style="background-color: #560bad" class="cardButton" onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+                </div>
+            `
+            listCard.appendChild(newDiv);
+        }
+
+        total.innerText = totalPrice.toLocaleString();
+        quantity.innerText = count;
+    })
+}
+
+const changeQuantity = (key, quantity) => {
+    if(quantity == 0){
+        delete listCards[key];
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price
+    }
+    reloadCard()
+}
